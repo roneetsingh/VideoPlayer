92e408d9a4f55f0be989a9c3d1494442f8671c5d
roneetsingh <kaket786@gmail.com>
2021-11-24 11:25:27 +0530

    Assignment Completed

--- Player/views.py
+++ Player/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from Player.models import Video
+
 # Create your views here.
+def index(request):
+    video=Video.objects.all()
+    return render(request, "index.html", {"video": video})
